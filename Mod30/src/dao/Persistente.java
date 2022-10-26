4edfa65260397f3b5d6149b82ea9848eddc3ad03
Pablo Câmara Aragon <pabloaragon7@gmail.com>
2022-10-26 10:32:21 -0300

    src

---
+++ Mod30/src/dao/Persistente.java
@@ -0,0 +1,11 @@
+package br.com.rpires.dao;
+
+
+public interface Persistente {
+
+    //public Long getCodigo();
+
+    public Long getId();
+
+    public void setId(Long id);
+}
---
+++ Mod30/src/services/IProdutoService.java
@@ -0,0 +1,9 @@
+package services;
+
+import br.com.rpires.domain.Produto;
+import br.com.rpires.services.generic.IGenericService;
+
+
+public interface IProdutoService extends IGenericService<Produto, String> {
+
+}
